907b99d9b973c8a7a7428a8595d964a7cd0a4842
David Alejandro Trejo Pizzo <dtrejopizzo@gmail.com>
2024-06-13 14:54:28 +0200

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
       </p>
       <div className="fixed bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:w-auto lg:bg-none lg:mb-0">
         <a
-          href="https://www.llamaindex.ai/"
+          href="https://clippy.fyi/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
         >
         </a>
